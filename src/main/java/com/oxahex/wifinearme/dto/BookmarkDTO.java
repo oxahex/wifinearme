cea65dcc61a1dccd1b8f2fcbee85ed1c7e0c0772
oxahex <oxahex@gmail.com>
2023-09-02 15:15:45 +0900

    feat: 북마크, 북마크 그룹 DTO 클래스 생성

---
+++ src/main/java/com/oxahex/wifinearme/dto/BookmarkDTO.java
@@ -0,0 +1,19 @@
+package com.oxahex.wifinearme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class BookmarkDTO {
+    private Integer id;
+    private Timestamp createTimestamp;
+    private WifiDTO wifi;
+    private BookmarkGroupDTO bookmarkGroup;
+}
