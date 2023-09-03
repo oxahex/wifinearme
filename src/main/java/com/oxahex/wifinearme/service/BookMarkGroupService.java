cc60bb4f2a124d50d61cfaebb53c567326ad1022
oxahex <oxahex@gmail.com>
2023-09-03 13:43:37 +0900

    feat: 북마크 순서 정렬 기능 구현
    - 북마크 그룹 생성 시 테이블에 같은 순서값이 존재하는지 확인하는 기능 추가
    - 유저가 입력한 순서와 같거나 큰 행의 view_order 값을 하나씩 증가시키는 기능 추가

--- src/main/java/com/oxahex/wifinearme/service/BookMarkGroupService.java
+++ src/main/java/com/oxahex/wifinearme/service/BookMarkGroupService.java
@@ -18,15 +18,21 @@ public class BookMarkGroupService {
         PreparedStatement pstmt = null;
         int affected = 0;
 
+        String name = bookmarkGroup.getName();
+        int order = bookmarkGroup.getOrder();
+        Timestamp createTimestamp = bookmarkGroup.getCreateTimestamp();
+
         try {
+            if (isOrderDuplicated(order)) updateOrder(order);
+
             String sql = " insert into bookmark_group           "+"\n"
                         +" (name, view_order, create_timestamp)    "+"\n"
                         +" values (?, ?, ?)                        ";
 
             pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, bookmarkGroup.getName());
-            pstmt.setInt(2, bookmarkGroup.getOrder());
-            pstmt.setTimestamp(3, bookmarkGroup.getCreateTimestamp());
+            pstmt.setString(1, name);
+            pstmt.setInt(2, order);
+            pstmt.setTimestamp(3, createTimestamp);
 
             affected = pstmt.executeUpdate();
         } catch (SQLException e) {
@@ -82,4 +88,70 @@ public class BookMarkGroupService {
         return bookmarkGroupList;
     }
 
+    /**
+     * 유저가 입력한 순서와 같거나 큰 행의 view_order + 1
+     * @param userOrder 유저 입력 순서
+     * @return 순서 업데이트 수행 결과 반환
+     */
+    private boolean updateOrder(int userOrder) {
+        Connection conn = DBManager.getConnection();
+        PreparedStatement pstmt = null;
+        int affected = 0;
+
+        try {
+            String sql = " update bookmark_group                "+"\n"
+                        +" set view_order = view_order + 1      "+"\n"
+                        +" where view_order >= ?                ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, userOrder);
+            affected = pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("updateBookmarkGroupOrder: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            DBManager.closeConnection(pstmt);
+            DBManager.closeConnection(conn);
+        }
+
+        return affected == 1;
+    }
+
+    /**
+     * DB에 유저가 생성하고자 하는 북마크 그룹 순서와 동일한 순서가 있는지 확인
+     * @param userOrder 생성하려는 북마크 그룹의 순서
+     * @return 동일한 값이 존재하면 true, 없으면 false
+     */
+    private boolean isOrderDuplicated(int userOrder) {
+        Connection conn = DBManager.getConnection();
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = " select exists (           "+"\n"
+                        +"   select *               "+"\n"
+                        +"   from bookmark_group    "+"\n"
+                        +"   where view_order = ?   "+"\n"
+                        +" ) as row_exist           ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, userOrder);
+
+            rs = pstmt.executeQuery();
+            if (rs.next() && rs.getBoolean("row_exist")) return true;
+
+        } catch (SQLException e) {
+            System.out.println("isBookmarkGroupOrderDuplicated: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            DBManager.closeConnection(rs);
+            DBManager.closeConnection(pstmt);
+            DBManager.closeConnection(conn);
+        }
+
+        return false;
+    }
 }
